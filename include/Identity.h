4721c1d88acb3730366f99fd8b6b9f20315431a5
SheepBowwow <alin989877@gmail.com>
2023-09-12 08:38:26 +0800

    添加了Identity.h

---
+++ include/Identity.h
@@ -0,0 +1,15 @@
+#ifndef  IDENTITY_H
+#define IDENTITY_H
+
+#include <iostream>
+using namespace std;
+
+class Identity
+{
+public:
+    virtual void openMenu() = 0;
+    string u_Name;
+    string u_Pwd;
+};
+
+#endif
